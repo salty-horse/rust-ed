b34cac0ae772901e71f90c49ed399489bcb74ba4
Alice Maz <alice@alicemaz.com>
2016-02-20 15:10:30 -0800

    finally some progress

--- src/main.rs
+++ src/main.rs
@@ -89,72 +89,92 @@ impl Editor {
                 let mut addr_mode = true;
                 let mut addr_offset_mode = false;
 
-                let mut left_addr = 0;
-                let mut right_addr = 0;
+                //various commands have their own default addresses
+                //but 0 is valid input in some cases
+                //so -1 communicates nil unambiguously
+                let mut left_addr = -1;
+                let mut right_addr = -1;
+                let mut curr_addr = self.current_line;
+                let mut expect_tail = false;
 
-                let mut p_flag = false;
-                let mut n_flag = false;
-                let mut l_flag = false;
+                let mut i = 0;
+                while i < line.len() {
+                    let c = line.char_at(i);
 
-                //FIXME address must handle arbitrary whitespace
-                let mut chars = line.trim_left().chars().peekable();
-
-                //address base
-                //FIXME this is dumb but I am tired
-                let c = if chars.peek().is_some() {
-                    *(chars.peek().unwrap())
-                } else {
-                    panic!("this shouldn't happen")
-                };
+                    //ignore spacing, break on end of address
+                    if c == ' ' || c == '\t' {
+                        continue;
+                    } else if is_command(c) || c == '\n' {
+                        break;
+                    }
                     
                 match c {
+                    '\n' | (_ if is_command(c)) => break,
+                    //'\n' => break,
+                    ' ' | '\t' => continue,
                     '.' => {
-                        chars.next();
-                        right_addr = self.current_line;
+                        if expect_tail {
+                            panic!("return error");
+                        } else {
+                            left_addr = right_addr;
+                            right_addr = curr_addr;
+                            expect_tail = true;
+                        }
                     },
                     '$' => {
-                        chars.next();
-                        right_addr = self.line_buffer.len();
+                        if expect_tail {
+                            panic!("return error");
+                        } else {
+                            left_addr = right_addr;
+                            right_addr = self.line_buffer.len();
+                            expect_tail = true;
+                        }
                     },
-                    '%' | ',' => {
-                        chars.next();
-                        left_addr = 1;
-                        right_addr = self.line_buffer.len();
+                    //like 1,$
+                    '%' => {
+                        if expect_tail {
+                            panic!("return error");
+                        } else {
+                            left_addr = 1;
+                            right_addr = self.line_buffer.len();
+                            expect_tail = true;
+                        }
+                    },
+                    ',' => {
+                        //if true, delimiter. else like 1,$
+                        if expect_tail {
+                            expect_tail = false;
+                        } else {
+                            left_addr = 1;
+                            right_addr = self.line_buffer.len();
+                            expect_tail = true;
+                        }
                     },
                     //FIXME this entire arm is cringe-inducing
                     n if n.is_digit(10) => {
-                        chars.next();
+
                         let mut num = n.to_digit(10).unwrap() as isize;
 
-                        loop {
-                            println!("n: {}, num: {}", n, num);
-                            //FIXME this is dumb
-                            let n = if chars.peek().is_some() {
-                                *(chars.peek().unwrap())
-                            } else {
-                                panic!("this shouldn't happen")
-                            };
-
-                            match n {
-                                n if n.is_digit(10) => {
-                                    chars.next();
-                                    num = num * 10 + (n.to_digit(10).unwrap() as isize);
-                                },
-                                _ => break,
-                                //None => panic!("this shouldn't happen")
-                            }
+                        //FIXME in C this would be a simple test on an assignment in parens
+                        //look into while let, maybe that is what that is for
+                        while line.char_at(i+1).is_some() && line.char_at(i+1).unwrap().is_digit(10) {
+                            i += 1;
+                            num = num * 10 + (line.char_at(i).unwrap().to_digit(10).unwrap() as isize);
                         }
 
-                        //FIXME is all this isize/usize stuff sane
-                        //annoying bc I was usize for array access
-                        //but need signed int for < 0 during (but never after) math
-                        //or... rearrange so that never matters? hm
-                        if num < 1 || (num as usize) > self.line_buffer.len() {
-                            println!("num is {}", num);
-                            panic!("return error");
+                        //if true, positive offset. else assignment
+                        //in both cases don't check validity until the end
+                        //something like 1-500+500 is valid
+                        if expect_tail {
+                            right_addr += num;
+                        } else {
+                            left_addr = right_addr;
+                            right_addr = num;
+                            expect_tail = true;
                         }
-
-                        right_addr = num as usize;
+/* TODO TODO when I am back, match +|- and do the same as the above basically
+set bool for add/subtract, strip whitespace, load in the number if exists or plusminus 1 otherwise
+also check whether it should change the current addr in place or if it just waits for the end
                     },
                     '\'' => {
                         chars.next();
