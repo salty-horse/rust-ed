dea32bf1ebd2d6c7270d3a4de56a4328c88913f5
Alice Maz <alice@alicemaz.com>
2016-02-21 22:06:58 -0800

    wheee address parsing actually kinda works

--- src/main.rs
+++ src/main.rs
@@ -92,9 +92,12 @@ impl Editor {
                 //various commands have their own default addresses
                 //but 0 is valid input in some cases
                 //so -1 communicates nil unambiguously
-                let mut left_addr = -1;
-                let mut right_addr = -1;
-                let mut curr_addr = self.current_line;
+                //FIXME wait shit no it doesn't
+                //weh think about how to fix w/o even more bools
+                //I don't think rust lets you check if uninitialized sigh
+                let mut left_addr: isize = -1;
+                let mut right_addr: isize = -1;
+                let mut curr_addr: isize = self.current_line as isize;
                 let mut expect_tail = false;
 
                 let mut i = 0;
@@ -108,92 +111,115 @@ impl Editor {
                         break;
                     }
                     
-                match c {
-                    '\n' | (_ if is_command(c)) => break,
-                    //'\n' => break,
-                    ' ' | '\t' => continue,
-                    '.' => {
-                        if expect_tail {
-                            panic!("return error");
-                        } else {
-                            left_addr = right_addr;
-                            right_addr = curr_addr;
-                            expect_tail = true;
-                        }
-                    },
-                    '$' => {
-                        if expect_tail {
-                            panic!("return error");
-                        } else {
-                            left_addr = right_addr;
-                            right_addr = self.line_buffer.len();
-                            expect_tail = true;
-                        }
-                    },
-                    //like 1,$
-                    '%' => {
-                        if expect_tail {
-                            panic!("return error");
-                        } else {
-                            left_addr = 1;
-                            right_addr = self.line_buffer.len();
-                            expect_tail = true;
-                        }
-                    },
-                    ',' => {
-                        //if true, delimiter. else like 1,$
-                        if expect_tail {
-                            expect_tail = false;
-                        } else {
-                            left_addr = 1;
-                            right_addr = self.line_buffer.len();
-                            expect_tail = true;
-                        }
-                    },
-                    //FIXME this entire arm is cringe-inducing
-                    n if n.is_digit(10) => {
+                    match c {
+                        '\n' | _ if is_command(c) => break,
+                        //'\n' => break,
+                        ' ' | '\t' => continue,
+                        '.' => {
+                            if expect_tail {
+                                panic!("return error");
+                            } else {
+                                left_addr = right_addr;
+                                right_addr = curr_addr;
+                                expect_tail = true;
+                            }
+                        },
+                        '$' => {
+                            if expect_tail {
+                                panic!("return error");
+                            } else {
+                                left_addr = right_addr;
+                                right_addr = self.line_buffer.len() as isize;
+                                expect_tail = true;
+                            }
+                        },
+                        //like 1,$
+                        '%' => {
+                            if expect_tail {
+                                panic!("return error");
+                            } else {
+                                left_addr = 1;
+                                right_addr = self.line_buffer.len() as isize;
+                                expect_tail = true;
+                            }
+                        },
+                        ',' => {
+                            //if true, delimiter. else like 1,$
+                            if expect_tail {
+                                expect_tail = false;
+                            } else {
+                                left_addr = 1;
+                                right_addr = self.line_buffer.len() as isize;
+                                expect_tail = true;
+                            }
+                        },
+                        //FIXME this entire arm is cringe-inducing
+                        n if n.is_digit(10) => {
 
-                        let mut num = n.to_digit(10).unwrap() as isize;
+                            let mut num = n.to_digit(10).unwrap() as isize;
 
-                        //FIXME in C this would be a simple test on an assignment in parens
-                        //look into while let, maybe that is what that is for
-                        while line.char_at(i+1).is_some() && line.char_at(i+1).unwrap().is_digit(10) {
-                            i += 1;
-                            num = num * 10 + (line.char_at(i).unwrap().to_digit(10).unwrap() as isize);
-                        }
+                            //FIXME in C this would be a simple test on an assignment in parens
+                            //look into while let, maybe that is what that is for
+                            while line.char_at(i+1).is_digit(10) {
+                                i += 1;
+                                num = num * 10 + (line.char_at(i).to_digit(10).unwrap() as isize);
+                            }
 
-                        //if true, positive offset. else assignment
-                        //in both cases don't check validity until the end
-                        //something like 1-500+500 is valid
-                        if expect_tail {
-                            right_addr += num;
-                        } else {
-                            left_addr = right_addr;
-                            right_addr = num;
-                            expect_tail = true;
-                        }
-/* TODO TODO when I am back, match +|- and do the same as the above basically
-set bool for add/subtract, strip whitespace, load in the number if exists or plusminus 1 otherwise
-also check whether it should change the current addr in place or if it just waits for the end
-                    },
-                    '\'' => {
-                        chars.next();
-                        let c = chars.next();
-                        if c.is_some() {
-                            let c = c.unwrap();
-                            if c.is_alphabetic() {
+                            //if true, positive offset. else assignment
+                            //in both cases don't check validity until the end
+                            //something like 1-500+500 is valid
+                            if expect_tail {
+                                right_addr += num;
+                            } else {
+                                left_addr = right_addr;
+                                right_addr = num;
+                                expect_tail = true;
+                            }
+                        },
+                        s if s == '+' || s == '-' => {
+                            let sign = if s == '+' {1} else {-1};
+
+                            //FIXME ughhhh this is so ugly
+                            //but, after the +/- increment past whitespace
+                            while (line.char_at(i+1) == ' ' || line.char_at(i+1) == '\t') {
+                                i += 1;
+                            }
+
+                            //now get number that may or may not follow the +/-
+                            //bool is necessary because +0 is valid
+                            //but if there's no num we act like +1
+                            let mut num = 0;
+                            let mut got_num = false;
+                            while line.char_at(i+1).is_digit(10) {
+                                i += 1;
+                                num = num * 10 + (line.char_at(i).to_digit(10).unwrap() as isize);
+                                got_num = true;
+                            }
+
+                            num = if got_num {num * sign} else {1};
+
+                            //similar to with number match
+                            if expect_tail {
+                                right_addr += num;
+                            } else {
+                                left_addr = right_addr;
+                                right_addr = curr_addr;
+                                expect_tail = true;
+                            }
+                        },
+                        '\'' => {
+                            if line.char_at(i+1).is_alphabetic() {
+                                i += 1;
                                 panic!("todo: marks");
                             } else {
                                 panic!("return error");
                             }
-                        }
-                    },
-                    '/' | '?' => panic!("todo: regex mode"),
-                    _ => { ;},
-                    //None => panic!("this shouldn't happen")
-                }
+                        },
+                        '/' | '?' => panic!("todo: regex mode"),
+                        _ => { ;},
+                    }
                 
-                println!("left: {}, right: {}", left_addr, right_addr);
+                    i += 1;
 
                 addr_offset_mode = true;
 /* come back to this tmrw
@@ -203,6 +229,9 @@ also check whether it should change the current addr in place or if it just wait
                     }
                 }
 */
+                }
+
+                println!("left: {}, right: {}", left_addr, right_addr);
             },
             Mode::Insert => {
             }
